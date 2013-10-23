7abaf9a12112c9471c2bfda187b3d52a4efb489f
cuixu66 <cuixu66@hotmail.com>
2013-10-23 04:48:15 -0400

    setup.sh is ready for use

---
+++ setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+cd ${HOME}
+
+new_dir=dotfiles
+old_dir=dotfiles.old
+
+if [ -d ./${new_dir}/ ] ; then
+    mv ${new_dir} ${old_dir}
+else
+    mkdir ${old_dir}
+fi
+
+git clone https://github.com/cuixu66/dotfiles
+
+create_sym_link() {
+# ln -sb original_file sym_link
+# above will work on ubuntu but not on osx
+# -s == --symbolic on ubuntu
+# -b == --backup on ubuntu
+
+# -f is testing file existence
+# -d is testing directory existence
+# -L is testing symbolic link existence
+    if [ -f ${1} ] || [ -d ${1} ] || [ -L ${1} ]
+    then
+	mv ${1} ${old_dir}/${1}.old
+    fi
+    ln -s ${new_dir}/${1} .
+}
+
+create_sym_link .screenrc
+create_sym_link .bash_profile
+create_sym_link .bashrc
+create_sym_link .bashrc_custom
+create_sym_link .dir_colors
+create_sym_link .emacs.d
+
+# purge old emacs AVOIDING this due to fragile server setup of MicroFuge for now
+# sudo apt-get update
+# sudo apt-get install
+# sudo apt-get purge emacs emacs-snapshot-common emacs-snapshot-bin-common emacs-snapshot emacs-snapshot-el emacs-snapshot-gtk emacs23 emacs23-bin-common emacs23-common emacs23-el emacs23-nox emacs23-lucid auctex apel emacs24 emacs24-bin-common emacs24-common emacs24-common-non-dfsg
+
+# Install emacs24
+# https://launchpad.net/~cassou/+archive/emacs
+sudo add-apt-repository -y ppa:cassou/emacs
+sudo apt-get -qq update
+sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
+
